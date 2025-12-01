194578669699b4e2f254cded0c6a701af67873f0
dkoklev <danikoklef@gmail.com>
2025-12-01 16:35:08 +0200

    Added additional fields for booking template and generated docs

--- internal/provider/booking_template_resource_test.go
+++ internal/provider/booking_template_resource_test.go
@@ -75,6 +75,9 @@ func TestAccBookingTemplateResource(t *testing.T) {
 					resource.TestCheckResourceAttr("udoma_booking_template.test", "env_vars.FOO", "bar"),
 					resource.TestCheckResourceAttr("udoma_booking_template.test", "env_vars.TEST2", "value"),
 
+					resource.TestCheckResourceAttr("udoma_booking_template.test", "trigger_source", "invoice"),
+
+					resource.TestCheckResourceAttr("udoma_booking_template.test", "init_script", "// This is a test init script"),
 
 					resource.TestCheckResourceAttr("udoma_booking_template.test", "script", "// This is a test generation script"),
 				),
@@ -123,6 +126,7 @@ func resourceDefinitionBookingTemplate(name, description string) string {
 				}
 			]
 		}
+
 		env_vars = {
 			FOO   = "bar"
 			TEST2 = "value"
